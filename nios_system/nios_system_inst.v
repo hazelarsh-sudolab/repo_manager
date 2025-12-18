d301bdf3d911a5c5063fc2a95ad212245eb9bc8c
hazelarsh-sudolab <hazelarsh@gmail.com>
2025-12-18 12:28:58 +0800

    Delete nios_system directory

--- nios_system/nios_system_inst.v
+++
@@ -1,9 +0,0 @@
-	nios_system u0 (
-		.clk_clk         (<connected-to-clk_clk>),         //      clk.clk
-		.reset_reset_n   (<connected-to-reset_reset_n>),   //    reset.reset_n
-		.gpio0_export    (<connected-to-gpio0_export>),    //    gpio0.export
-		.hex0_export     (<connected-to-hex0_export>),     //     hex0.export
-		.leds_export     (<connected-to-leds_export>),     //     leds.export
-		.switches_export (<connected-to-switches_export>)  // switches.export
-	);
-
